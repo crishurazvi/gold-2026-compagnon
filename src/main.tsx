eb1ab09bd3dd3af1ed6350d0609fb65387c0d791
crishurazvi <razvan9667@protonmail.com>
2026-02-20 23:41:21 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+import './index.css' 
 
+// IMPORTANT: Aici se face legătura cu <div id="root"> din HTML
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
